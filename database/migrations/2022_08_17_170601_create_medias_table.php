7b41fd41645b754fd281a28288dc10c78df3064e
Khalifa Ababacar Diene <khalifadiene95@gmail.com>
2022-08-17 17:10:42 +0000

    Table media ajoutée

---
+++ database/migrations/2022_08_17_170601_create_medias_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medias', function (Blueprint $table) {
+            $table->id();
+            $table->string('lien');
+            $table->boolean('status')->default(1);
+            $table->unsignedBigInteger('article_id');
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medias');
+    }
+}
